a5faee5312698eafdd7a6b99727d2fca5b3e9c62
Bhargavi <bhargaviambre1605@gmail.com>
2026-06-24 19:05:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const A=5;
+const B=10;
+
+if(A % 2 == 0){
+    console.log("The number " +A+ " is even");
+}
+else{
+    console.log("The number " +A+ " is odd");
+}
+if(B % 2 ==0){
+    console.log("The number " +B+ " is even" );
+}
+else{
+    console.log("The number "+B+ " is odd");
+}
+
